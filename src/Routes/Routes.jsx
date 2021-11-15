ee43439203c861cbe88c76b271a199b3afc3c883
ImDavidCO <davorgiiii@gmail.com>
2021-11-15 13:34:20 -0500

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+
+} from "react-router-dom";
+import PrivateLayout from "../Layout/PrivateLayout";
+import PublicLayout from "../Layout/PublicLayout";
+import DashBoard from "../Pages/Admin/DashBoard";
+import Roles from "../Pages/Admin/Roles";
+import Ventas from "../Pages/Admin/Ventas";
+import Index from "../Pages/Public/Index";
+
+const Routes = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path={['/dash', '/roles', '/ventas']}>
+                    <PrivateLayout>
+                        <Switch>
+                            <Route path='/dash'>
+                                <DashBoard/>
+                            </Route>
+                            <Route path='/roles'>
+                                <Roles/>
+                            </Route>
+                            <Route path='ventas'>
+                                <Ventas/>
+                            </Route>
+
+                        </Switch>
+                    </PrivateLayout>
+                </Route>
+                <Route path='/'>
+                    <PublicLayout>
+                        <Index/>
+                    </PublicLayout>
+                </Route>
+            </Switch>
+
+        </Router>
+            
+    )
+}
+
+export default Routes
